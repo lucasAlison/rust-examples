84dc63d5d5c14b9dcd11b9920ab6bf0e22e7264c
Lucas Alison de Lima <lucas.alison@orsegups.com.br>
2019-09-02 16:33:20 -0300

    format file

--- exception-example/src/main.rs
+++ exception-example/src/main.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::Read;
 
 fn main() {
-    match File::open("exemplo.txt") {
+    match File::open("./exemplo.txt") {
         Ok(mut file) => { // If file exist
             let mut txt = String::new();
             file.read_to_string(&mut txt).ok();
