c533c60cd363b9df1c2247994700869ae2d3717b
Lucas Alison de Lima <lucas.alison@orsegups.com.br>
2019-09-02 16:27:35 -0300

    add exception example

---
+++ exception-example/src/main.rs
@@ -0,0 +1,13 @@
+use std::fs::File;
+use std::io::Read;
+
+fn main() {
+    match File::open("exemplo.txt") {
+        Ok(mut file) => { // If file exist
+            let mut txt = String::new();
+            file.read_to_string(&mut txt).ok();
+            println!("{}", txt);
+        }
+        Err(err) => println!("Erro: {}", err), // If file not exist
+    }
+}
