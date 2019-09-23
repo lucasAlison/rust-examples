8372e0cb3fbaf1d309b4a31b0af1a8d79c285763
Lucas Alison de Lima <lucas.alison@orsegups.com.br>
2019-09-23 17:39:42 -0300

    add struct function example

--- rectangle/src/main.rs
+++ rectangle/src/main.rs
@@ -11,6 +11,9 @@ impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
 }
 
 fn main() {
@@ -27,4 +30,7 @@ fn main() {
 
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let square = Rectangle::square(4);
+    println!("rect1 is {:#?}", square);
 }
